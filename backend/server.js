c55d1ed8c59a5557d83abb982a6377fe56fde4cf
Mourya Manideep Kandregula <kmmd434389@gmail.com>
2026-04-11 20:42:13 +0530

    update url

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+// allow all origins (important for Vercel)
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from backend v4");
+  res.send("Hello from backend v2 🚀");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+// IMPORTANT: bind to public IP
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 function App() {
   const [msg, setMsg] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:5000")
-      .then(res => res.text())
-      .then(data => setMsg(data));
-  }, []);
-
+useEffect(() => {
+  fetch("http://your-ec2-ip:5000")
+    .then(res => res.text())
+    .then(data => setMsg(data))
+    .catch(err => console.error("ERROR:", err));
+}, []);
   return <h1>{msg}</h1>;
 }
 
